88d01258e8480e4eee65dccbe5c4909bee2e2381
cycugdsc <cycugdsc@gmail.com>
2025-05-09 18:40:29 +0000

    add icons.dart

---
+++ lib/icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainIcon extends StatelessWidget {
+  const MainIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'icons/Wandering.png',
+      width: 48,
+      height: 48,
+    );
+  }
+}
