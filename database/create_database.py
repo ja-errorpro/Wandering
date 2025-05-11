7eb16555e3decaa988c2d33412604d25a09a9907
Sapphire <jefflan333@gmail.com>
2025-05-11 16:25:56 +0800

    add main railway station to find attactions

--- database/create_database.py
+++ database/create_database.py
@@ -5,13 +5,44 @@ from collections import deque
 import json
 
 API_KEY = "AIzaSyAUBEI-N5zgzAmFbDcyoU-gdPiiPmuw-i8"
-
-INITIAL_LOCATION = (25.047924, 121.517081) # 台北車站
+# "Taipei Main Station": "25.047924, 121.517081",  #台北車站
+INITIAL_LOCATION = [("Keelung North Railway Station", "25.134274343711827, 121.74005245320404"), # 基隆北站
+                    ("Qi Du Railway Station", "25.09468489648192, 121.7127353316717"),  # 七堵車站
+                    ("Banqiao Railway Station", "25.014196157791464, 121.46380554247673"), # 板橋車站
+                    ("Shulin Railway Station", "24.991224628773146, 121.42488153989889"), # 樹林車站
+                    ("Ruifang Railway Station", "25.10882632614538, 121.80596281436307"), # 瑞芳車站
+                    ("Nangang Railway Station", "25.05326442718221, 121.60700774062991"), # 南港車站
+                    ("Songshan Railway Station", "25.049369808852937, 121.57825488420708"), # 松山車站
+                    ("Wanhua Railway Station", "25.03343177648604, 121.50036814247721"), # 萬華車站
+                    ("Taoyuan Railway Station", "24.989085294212295, 121.31449364062816"), # 桃園車站
+                    ("Zhongli Railway Station", "24.95377498709887, 121.2260759940431"), # 中壢車站
+                    ("Hsinchu Railway Station", "24.801762984627317, 120.97164194062309"), # 新竹車站
+                    ("Zhunan Railway Station", "24.686764446943904, 120.88091674801105"), # 竹南車站
+                    ("Miaoli Railway Station", "24.5701760960207, 120.82272923451403"), # 苗栗車站
+                    ("Fengyuan Railway Station", "24.25433860549813, 120.72386783450546"), # 豐原車站
+                    ("Taichung Railway Station", "24.13799179416188, 120.69001556678998"), # 台中車站
+                    ("Changhua Railway Station", "24.081770504061218, 120.53891433819653"), # 彰化車站
+                    ("Yuanlin Railway Station", "23.95944563962951, 120.5700433363456"), # 員林車站
+                    ("Douliu Railway Station", "23.71209312112982, 120.54088173689814"), # 斗六車站
+                    ("Chiayi Railway Station", "23.479366532718625, 120.44116028873985"), # 嘉義車站
+                    ("Xinying Railway Station", "23.30691479263617, 120.32333098845577"), # 新營車站
+                    ("Tainan Railway Station", "22.997293478277427, 120.21336943447264"), # 台南車站
+                    ("Okayama Train Station", "22.792388672149162, 120.3005671669982"), # 岡山車站
+                    ("Xinzuoying Railway Station", "22.68898138339625, 120.30843314053197"), # 新左營車站
+                    ("Kaohsiung Railway Station", "22.63968051963547, 120.30295850562814"), # 高雄車站
+                    ("Pingtung Railway Station", "22.66906577932277, 120.48645913446433"), # 屏東車站
+                    ("Chaozhou Railway Station", "22.550186747858906, 120.53660672863839"), # 潮州車站
+                    ("Taitung Railway Station", "22.794093753146733, 121.1234566191258"), # 台東車站
+                    ("Yuli Railway Station", "23.331677603167147, 121.31212769215196"), # 玉里車站
+                    ("Hualien Railway Station", "23.99345050338126, 121.60163173449853"), # 花蓮車站
+                    ("Su'ao New Railway Station", "24.609203021467092, 121.82880932713128"), # 蘇澳新車站
+                    ("Yilan Railway Station", "24.755448164274277, 121.75876800753154") # 宜蘭車站
+                    ]
 RADIUS = 10000
 PLACE_TYPE = "tourist_attraction"
 LANGUAGE = "zh-TW"
 MAX_RESULTS = 1000
-OUTPUT_FILE = "taiwan_attractions.csv"
+OUTPUT_FILE = "_attractions_1000.csv"
 
 def get_places_in_radius(location, radius, place_type, api_key, language):
     """
@@ -22,7 +53,7 @@ def get_places_in_radius(location, radius, place_type, api_key, language):
     places_result = gmaps.places_nearby(location=location, radius=radius, type=place_type, language=language)
     return places_result['results'], places_result.get('next_page_token')
 
-def save_to_csv(data, filename=OUTPUT_FILE):
+def save_to_csv(data, filename):
     """
     將景點資料儲存到 CSV 檔案
     """
@@ -76,7 +107,7 @@ def expand_search(initial_location, radius, place_type, api_key, language):
     """
     found_places = []
     visited_locations = set()
-    search_queue = deque([(initial_location, initial_location, radius)])
+    search_queue = deque([(initial_location[1], initial_location[1], radius)])
 
     while search_queue and len(found_places) < MAX_RESULTS:
         location, original_location, original_radius = search_queue.popleft()
@@ -109,7 +140,8 @@ def expand_search(initial_location, radius, place_type, api_key, language):
             break
 
     print(f"總共找到 {len(found_places)} 個景點。")
-    save_to_csv(found_places) # 使用 save_to_csv 儲存資料
+    save_to_csv(found_places, initial_location[0] + OUTPUT_FILE) # 使用 save_to_csv 儲存資料
 
 if __name__ == "__main__":
-    expand_search(INITIAL_LOCATION, RADIUS, PLACE_TYPE, API_KEY, LANGUAGE)
+    for location in INITIAL_LOCATION:
+        expand_search(location, RADIUS, PLACE_TYPE, API_KEY, LANGUAGE)
